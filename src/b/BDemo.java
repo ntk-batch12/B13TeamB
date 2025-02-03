5d5c1cad8074e3518b93a59e97a629ec256c9859
elkha <elkha@10.0.0.142>
2025-02-02 23:31:19 -0800

    just practicing

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -4,25 +4,15 @@ public class BDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamB");
+		 getNum(22,44);
+         getNum(11,66);
 	}
 	public static void addNum(int a, int b) {
 		System.out.println("team work");
 	}
+	public static void getNum(int a,int b) {
+		System.out.println(a+b);
+	}
+	
 
-	public static void divide(int a , int b) {
-		System.out.println(a/b);
-	}
-	
-public void add(int c, int d) {
-		
-		System.out.println(c + d);
-		
-	}
-	
-	
-	
- public static void multiply (int m, int n) {
-	 
-	 System.out.println( "print this");
- }
 }
