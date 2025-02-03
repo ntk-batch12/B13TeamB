69734bc7370d9750d3efaa903ceb9a14c9f04b60
ivancollazoo <collazoivan1412@gmail.com>
2025-02-02 20:30:19 -0400

    First commit

--- src/a/ADemo.java
+++ src/a/ADemo.java
@@ -6,6 +6,19 @@ public class ADemo {
 		System.out.println("ADemo: TeamB");
 		
 		System.out.println("Let's learn some new stuff!");
+		
+		int lakers = 122;
+		int celtics = 121;
+		
+		if(lakers > celtics) {
+			System.out.println("Lakers win");
+		} else {
+			System.out.println("Celtics win");
+		}
+		
+		
+		
+		
 	}
 	public static int getNum(int a,int b) {
 		int result=2*2;
@@ -16,5 +29,7 @@ public class ADemo {
 		
 		return result;
 	}
-
+	
+	
+	
 }
