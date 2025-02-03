262e3c53a52dd32561f99f0e524f057ac509eeb9
elkha <elkha@10.0.0.142>
2025-02-02 18:34:20 -0800

    added addNum() with Team

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,5 +5,8 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamB");
 	}
+	public static void addNum(int a, int b) {
+		System.out.println("team work");
+	}
 
 }
