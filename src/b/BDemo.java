beb73aad621fe17eff158b80ecd8446b2d89112c
Uncle Ricco <eric4cali@gmail.com>
2025-02-07 16:24:32 -0800

    Update BDemo.java

--- src/b/BDemo.java
+++ src/b/BDemo.java
@@ -5,30 +5,6 @@ public class BDemo {
 	public static void main(String[] args) {
 		System.out.println("BDemo: TeamB");
 	}
-	public static void addNum(int a, int b) {
-		System.out.println("team work");
-	}
-
-	public static void divide(int a , int b) {
-		System.out.println(a/b);
-	}
 	
-public void add(int c, int d) {
-		
-		System.out.println(c + d);
-		
-	}
-System.out.println("What er you doing?");
-public void division( int x , int y) {
-	System.out.println(x / y); 
-}
-
-public void remainder( int x , int y) {
-	System.out.println(x % y); 
-
-
-	System.out.println("What er you doing?");
-	System.out.println("What are you doing?");
-	System.out.println("What er you doing?");
 }
 }
